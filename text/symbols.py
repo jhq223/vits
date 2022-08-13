a22825ac9a7892978053d0049e4d4bdf803cedec
今何求 <62083936+jhq223@users.noreply.github.com>
2022-08-14 01:22:13 +0800

    use ja

--- text/symbols.py
+++ text/symbols.py
@@ -4,11 +4,10 @@
 Defines the set of symbols used in text input to the model.
 '''
 
-'''# japanese_cleaners
+# japanese_cleaners
 _pad        = '_'
 _punctuation = ',.!?-'
 _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧ↓↑ '
-'''
 
 '''# japanese_cleaners2
 _pad        = '_'
@@ -16,11 +15,11 @@ _punctuation = ',.!?-'
 _letters = 'AEINOQUabdefghijkmnoprstuvwyzʃʧʦ↓↑ '
 '''
 
-# korean_cleaners
+'''# korean_cleaners
 _pad        = '_'
 _punctuation = ',.!?…~'
 _letters = 'ㄱㄴㄷㄹㅁㅂㅅㅇㅈㅊㅋㅌㅍㅎㄲㄸㅃㅆㅉㅏㅓㅗㅜㅡㅣㅐㅔ '
-
+'''
 
 # Export all symbols:
 symbols = [_pad] + list(_punctuation) + list(_letters)
